4a6d6ce8de48988dfd0a183a833b0ff13fd175a8
CalNHobsGotU <calnhobsgotu@CalNHobsGotUs-MacBook.local>
2022-07-27 14:52:35 -0400

    Remove cloud block

--- versions.tf
+++ versions.tf
@@ -1,19 +1,19 @@
-terraform {
+# terraform {
 
-  cloud {
-    organization = "hashicorp-learn"
+#   cloud {
+#     organization = "LPE1"
 
-    workspaces {
-      name = "learn-terraform-cloud"
-    }
-  }
+#     workspaces {
+#       name = "learn-terraform-cloud"
+#     }
+#   }
 
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.28.0"
-    }
-  }
+#   required_providers {
+#     aws = {
+#       source  = "hashicorp/aws"
+#       version = "~> 3.28.0"
+#     }
+#   }
 
-  required_version = ">= 0.14.0"
-}
+#   required_version = ">= 0.14.0"
+# }
